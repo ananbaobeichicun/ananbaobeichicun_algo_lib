33c6cdb24b589f3d70adb1570cda9b47f2cd8c22
ananbaobeichicun <47203967+ananbaobeichicun@users.noreply.github.com>
2025-09-20 13:31:09 +0800

    feat: sparse table range assign operation

--- lib/range_sum_container/sparse_table.h
+++ lib/range_sum_container/sparse_table.h
@@ -3,8 +3,13 @@
 #include <cassert>
 #include <vector>
 
-#include "group_concept.h"
+#include "../concepts/group_concept.h"
 #include "segment.h"
+
+template<std::unsigned_integral T>
+T saturating_sub(const T x, const T y) {
+    return x > y ? x - y : 0;
+}
 
 template<semigroup T>
     requires idempotent<T>
@@ -20,20 +25,36 @@ struct sparse_table {
                  && std::indirectly_copyable<std::ranges::iterator_t<R>, typename std::vector<T>::iterator>
     constexpr explicit sparse_table(const R &r): sparse_table(r.size()) {
         std::ranges::copy(r, data[0].begin());
-        for (size_t j = 1; j < std::__lg(_size) + 1; ++j)
-            for (size_t i = 0; i < _size; ++i)
-                if (i + (1 << j - 1) < _size)
-                    data[j][i] = data[j - 1][i] + data[j - 1][i + (1 << j - 1)];
+        for (size_t i = 1; i < std::__lg(_size) + 1; ++i)
+            for (size_t j = 0; j + (1uz << i) - 1 < _size; ++j)
+                update(i, j);
     }
 
-    constexpr size_t size() const {
+    void update(const size_t i, const size_t j) {
+        data[i][j] = data[i - 1][j] + data[i - 1][j + (1uz << (i - 1))];
+    }
+
+    [[nodiscard]] constexpr size_t size() const {
         return _size;
     }
 
-    constexpr T query(const segment<size_t> seg) const {
+    [[nodiscard]] constexpr T query(const segment<size_t> seg) const {
         assert(seg.len() && seg <= (segment<size_t>{0, _size}));
         const size_t l = std::__lg(seg.len());
-        return data[l][seg.b] + data[l][seg.e - (1 << l)];
+        return data[l][seg.b] + data[l][seg.e - (1uz << l)];
+    }
+
+    /**
+     * 区间赋值操作
+     * @param seg 赋值的下标范围
+     * @param val 赋成的值
+     * 复杂度 O(_size + seg.len() * log(_size))
+     */
+    constexpr void modify(const segment<size_t> seg, const T val) {
+        std::fill(data[0].begin() + seg.b, data[0].begin() + seg.e, val);
+        for (size_t i = 1; i < std::__lg(_size) + 1; ++i)
+            for (size_t j = saturating_sub(seg.b, 1uz << i); j + (1uz << i) - 1 < _size && j < seg.e; ++j)
+                update(i, j);
     }
 
     constexpr T operator[](size_t idx) {
